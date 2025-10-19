4a4aa4666a296406bd27269f2a81fe2a33777538
skadi <devagleo888@gmail.com>
2025-10-19 23:13:39 +0300

    Добавил сущности в базе для сервиса User Покрыл комментариями

---
+++ src/User/User.Domain/Entities/UserEntity.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace User.Domain
+{
+    [Table("users")]
+    public class UserEntity
+    {
+        /// <summary>
+        /// Id пользователя в базе в GUID
+        /// </summary>
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Логин пользователя, максимум 100 символов
+        /// </summary>
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Пароль в хэш виде
+        /// </summary>
+        [Required]
+        [MaxLength(256)]
+        public string PasswordHash { get; set; } = string.Empty;
+    }
+}
