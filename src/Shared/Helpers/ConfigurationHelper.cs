6291b211faaaf824868b4fb68b53d0bd32964fde
skadi <devagleo888@gmail.com>
2025-10-19 23:27:59 +0300

    Подключил вспомогательный сервис для фабрики контекста и класс конфигурации JWT в DI

---
+++ src/Shared/Helpers/ConfigurationHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Helpers
+{
+    /// <summary>
+    /// Вспомогательный класс сборки конфигурации из оносительного пути для миграций
+    /// Расчитываем на то, что структура проекта будет соблюдатся по схеме Solution\src\Project\Project.Layer
+    /// </summary>
+    public static class ConfigurationHelper
+    {
+        public static IConfigurationRoot BuildConfiguration(string relativeProjectPath)
+        {
+            //Вычисляем путь до текущего Project, отрезаем два уровня и переходим в папку с целевым репозиторием
+            var basePath = Path.Combine(Directory.GetCurrentDirectory(), relativeProjectPath);
+            return new ConfigurationBuilder().SetBasePath(basePath)
+                                             .AddJsonFile("appsettings.json", optional: false)
+                                             .AddEnvironmentVariables()
+                                             .Build();
+        }
+    }
+}
---
+++ src/Shared/Settings/JWTOptions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Settings
+{
+    /// <summary>
+    /// Класс хранения JWT параметров, для лёгкого подключения в DI
+    /// </summary>
+    public class JwtOptions
+    {
+        /// <summary>
+        /// Секрет, которым сервер авторизации подписывает JWT
+        /// </summary>
+        public string Secret { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Сервис, выдавший JWT
+        /// </summary>
+        public string Issuer { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Адрес сервера выдавшего JWT
+        /// </summary>
+        public string Audience { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Срок действия токена
+        /// Используется только на сервисе выдачи токенов
+        /// </summary>
+        public int ExpiryMinutes { get; set; } = 120;
+    }
+}
