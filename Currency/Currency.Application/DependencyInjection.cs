06835f7a7df7cfac9e159550ce666345d7850d75
skadi <devagleo888@gmail.com>
2025-10-20 01:04:57 +0300

    Добавил зависимости от Application в DI

---
+++ Currency/Currency.Application/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Currency.Application.Interfaces;
+using Currency.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+
+
+namespace Currency.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            // Регистрация всех сервисов бизнес-логики
+            services.AddScoped<ICurrencyService, CurrencyService>();
+            services.AddScoped<IFavoriteService, FavoriteService>();
+
+            // Бизнес-логика фонового сервиса обновления валют
+            services.AddScoped<ICurrencyUpdaterService, CurrencyUpdaterService>();
+
+
+            return services;
+        }
+    }
+}
