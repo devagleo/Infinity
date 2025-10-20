0ebc0920711a7832d0759e23ff0be6448287ba1a
skadi <devagleo888@gmail.com>
2025-10-20 03:21:32 +0300

    Добавил слой GRPC для Gateway

---
+++ src/Gateway/Gateway.Grpc/Services/GatewayCurrencyGrpcService.cs
@@ -0,0 +1,124 @@
+﻿using Gateway.Application.Interfaces;
+using Gateway.Application.Models;
+using Gateway.Currency.Grpc;
+using Gateway.Grpc.Mappers;
+using Grpc.Core;
+
+namespace Gateway.Grpc.Services
+{
+    /// <summary>
+    /// Реализация GRPC
+    /// </summary>
+    public class GatewayCurrencyGrpcService : GatewayCurrencyProtoService.GatewayCurrencyProtoServiceBase
+    {
+        private readonly ICurrencyGrpcClient _currencyClient;
+
+        public GatewayCurrencyGrpcService(ICurrencyGrpcClient currencyClient)
+        {
+            _currencyClient = currencyClient;
+        }
+
+        /// <summary>
+        /// Получить все актуальные валюты из базы
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override async Task<GetAllCurrenciesResponse> GetAllCurrencies(Empty request, ServerCallContext context)
+        {
+            var list = await _currencyClient.GetAllCurrenciesAsync(context.RequestHeaders.GetValue("Authorization")!);
+
+            return new GetAllCurrenciesResponse
+            {
+                Currencies = { list.Select(x => x.ToCurrencyGrpc()) }
+            };
+        }
+
+        /// <summary>
+        /// Получить валюту по GUID
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override async Task<CurrencyGrpcDTO> GetCurrencyById(GetCurrencyByIdRequest request, ServerCallContext context)
+        {
+            var dto = await _currencyClient.GetCurrencyByIdAsync(request.Id, context.RequestHeaders.GetValue("Authorization")!);
+            return dto?.ToCurrencyGrpc() ?? new CurrencyGrpcDTO();
+        }
+
+        /// <summary>
+        /// Получить список избранных валют пользователя
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override async Task<GetUserCurrenciesResponse> GetUserCurrencies(GetUserCurrenciesRequest request, ServerCallContext context)
+        {
+            var currencies = await _currencyClient.GetUserCurrenciesAsync(request.UserId, context.RequestHeaders.GetValue("Authorization")!);
+
+            var response = new GetUserCurrenciesResponse();
+            response.Currencies.AddRange(currencies.Select(c => new CurrencyGrpcDTO
+            {
+                Id = c.Id,
+                Name = c.Name,
+                Rate = (double)c.Rate
+            }));
+
+            return response;
+        }
+
+        /// <summary>
+        /// Добавить валюту в избранное пользователю
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override async Task<ModifyFavoriteCurrencyResponse> AddFavoriteCurrency(ModifyFavoriteCurrencyRequest request, ServerCallContext context)
+        {
+            try
+            {
+                var success = await _currencyClient.AddFavoriteCurrencyAsync(request.UserId, request.CurrencyId, context.RequestHeaders.GetValue("Authorization")!);
+                return new ModifyFavoriteCurrencyResponse
+                {
+                    Success = success,
+                    Message = success ? "Added" : "Ошибка при добавлении"
+                };
+            }
+            catch (Exception ex)
+            {
+                return new ModifyFavoriteCurrencyResponse
+                {
+                    Success = false,
+                    Message = ex.Message
+                };
+            }
+        }
+
+        /// <summary>
+        /// Удалить валюту из избранных у пользователя
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override async Task<ModifyFavoriteCurrencyResponse> RemoveFavoriteCurrency(ModifyFavoriteCurrencyRequest request, ServerCallContext context)
+        {
+            try
+            {
+                var success = await _currencyClient.RemoveFavoriteCurrencyAsync(request.UserId, request.CurrencyId, context.RequestHeaders.GetValue("Authorization")!);
+                return new ModifyFavoriteCurrencyResponse
+                {
+                    Success = success,
+                    Message = success ? "Removed" : "Ошибка удаления"
+                };
+            }
+            catch (Exception ex)
+            {
+                return new ModifyFavoriteCurrencyResponse
+                {
+                    Success = false,
+                    Message = ex.Message
+                };
+            }
+        }
+    }
+}
---
+++ src/Gateway/Gateway.Grpc/Services/GatewayUserGrpcService.cs
@@ -0,0 +1,101 @@
+﻿using Gateway.Application.Interfaces;
+using Gateway.User.Grpc;
+using Grpc.Core;
+
+namespace Gateway.Grpc.Services
+{
+    /// <summary>
+    /// Реализация GRPC
+    /// </summary>
+    public class GatewayUserGrpcService : GatewayUserProtoService.GatewayUserProtoServiceBase
+    {
+        private readonly IUserGrpcClient _userClient;
+
+        public GatewayUserGrpcService(IUserGrpcClient userClient)
+        {
+            _userClient = userClient;
+        }
+
+        /// <summary>
+        /// Получить пользователя по ID
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override async Task<UserGrpcDTO> GetUserById(GetUserByIdRequest request, ServerCallContext context)
+        {
+            var user = await _userClient.GetUserByIdAsync(request.UserId, context.RequestHeaders.GetValue("Authorization")!);
+            return new UserGrpcDTO
+            {
+                Id = user?.Id ?? "",
+                Name = user?.Username ?? ""
+            };
+        }
+
+
+        /// <summary>
+        /// Получить пользователя по логину
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override async Task<UserGrpcDTO> GetUserIdByLogin(GetUserIdByLoginRequest request, ServerCallContext context)
+        {
+            var user = await _userClient.GetUserIdByLoginAsync(request.Login, context.RequestHeaders.GetValue("Authorization")!);
+            return new UserGrpcDTO
+            {
+                Id = user?.Id ?? "",
+                Name = user?.Username ?? ""
+            };
+        }
+
+        /// <summary>
+        /// Зарегистрировать пользователя
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override async Task<RegisterResponse> Register(RegisterRequest request, ServerCallContext context)
+        {
+            var result = await _userClient.RegisterAsync(request.Username, request.Password);
+            return new RegisterResponse
+            {
+                Id = result?.Id ?? "",
+                Username = result?.Username ?? "",
+                Message = result?.Message ?? ""
+            };
+        }
+
+        /// <summary>
+        /// Авторизовать пользователя
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override async Task<LoginResponse> Login(LoginRequest request, ServerCallContext context)
+        {
+            var result = await _userClient.LoginAsync(request.Username, request.Password);
+            return new LoginResponse
+            {
+                Token = result?.Token ?? "",
+                Message = result?.Message ?? ""
+            };
+        }
+
+        /// <summary>
+        /// Логаут пользователя
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override async Task<LogoutResponse> Logout(LogoutRequest request, ServerCallContext context)
+        {
+            var result = await _userClient.LogoutAsync(request.Token);
+            return new LogoutResponse
+            {
+                Success = result?.Success ?? false,
+                Message = result?.Message ?? ""
+            };
+        }
+    }
+}
