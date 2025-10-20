b01314b652eaf22180e39d34a75fd46e8567b045
skadi <devagleo888@gmail.com>
2025-10-20 03:31:49 +0300

    Добавлен слой REST API Добалвена поддержка Docker в API и GRPC

---
+++ src/Gateway/Gateway.Api/Models/User/LoginRequestModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Gateway.Api.Models.User
+{
+    /// <summary>
+    /// Транспортная модель между API и GRPC клиентом
+    /// </summary>
+    public class LoginRequestModel
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
